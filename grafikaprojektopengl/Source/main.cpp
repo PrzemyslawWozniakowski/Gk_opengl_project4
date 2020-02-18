644efd43bbb298c8bc67fc799ca61107a3a7db5f
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2020-02-18 20:16:14 +0100

    added deleting pyramid vbo and vao

--- grafikaprojektopengl/Source/main.cpp
+++ grafikaprojektopengl/Source/main.cpp
@@ -139,6 +139,33 @@ int main()
 		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
 	};
 
+	float pvertices[] = {
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		 0.5f, -0.5f,  0.5f,  2.0f, 1.0f,  0.0f,
+		 0.5f, -0.5f,  -0.5f,  2.0f, 1.0f,  0.0f,
+		 0.0f, 0.5f,  0.0f,  2.0f, 1.0f,  0.0f,
+
+		 -0.5f, -0.5f,  0.5f,  -2.0f, 1.0f,  0.0f,
+		 -0.5f, -0.5f,  -0.5f, -2.0f, 1.0f,  0.0f,
+		 0.0f, 0.5f,  0.0f,  -2.0f, 1.0f,  0.0f,
+
+		 0.5f, -0.5f,  0.5f,  0.0f, 1.0f,  2.0f,
+		 -0.5f, -0.5f,  0.5f,  0.0f, 1.0f,  2.0f,
+		 0.0f, 0.5f, 0.0f,  0.0f, 1.0f,  2.0f,
+
+		 0.5f, -0.5f,  -0.5f,  0.0f, 1.0f,  -2.0f,
+		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  -2.0f,
+		 0.0f,	0.5f,  0.0f,  0.0f, 1.0f,  -2.0f,
+	};
+
 #pragma region Sphere
 
 
@@ -267,6 +294,20 @@ int main()
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	unsigned int pVBO, pVAO;
+	glGenVertexArrays(1, &pVAO);
+	glGenBuffers(1, &pVBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, pVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pvertices), pvertices, GL_STATIC_DRAW);
+	glBindVertexArray(pVAO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 
 
@@ -472,6 +513,18 @@ int main()
 		shader.setMat4("model", model);
 		shader.setVec3("objectColor", 0.9f, 0.9f, 0.9f);
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		
+		//===========================================================
+		
+		glBindVertexArray(pVAO);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(9.0f, 0.25f, -9.0f));
+		model = glm::scale(model, glm::vec3(1.5f));
+		model = glm::rotate(model, (float)glfwGetTime()/4, glm::vec3(0.0f, 1.0f, 0.0f));
+		shader.setMat4("model", model);
+		shader.setVec3("objectColor", 1.0f, 0.0f, 0.5f);
+		glDrawArrays(GL_TRIANGLES, 0, 18);
+
 		//========================================================
 		if (showPointLights && !offLights)
 		{
@@ -531,6 +584,9 @@ int main()
 	glDeleteBuffers(1, &fVBO);
 	glDeleteBuffers(1, &fEBO);
 	
+	glDeleteVertexArrays(1, &pVAO);
+	glDeleteBuffers(1, &pVBO);
+
 	glfwTerminate();
 	return 0;
 }
